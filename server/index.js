a8ab46bf4168934d8b3ae507e3536b15364fda07
Roberta Häggström <roberta.haggstrom@gmail.com>
2018-08-20 21:08:38 +0200

    express with hello world works

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const path = require('path');
 
+// For deploying, so client code is served by our backend server.
+//app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(3001, () => console.log('Example app listening on port 3001!'))
 
-
-module.exports = app;
+//module.exports = app;
+// start this with: node index.js
