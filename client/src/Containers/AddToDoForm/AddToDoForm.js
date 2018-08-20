35a1e8aff21a0c71b1269fc998e8c87daaa948f7
Roberta Häggström <roberta.haggstrom@gmail.com>
2018-08-20 15:08:20 +0200

    more front end styling for ppages mobile view

--- client/src/Containers/AddToDoForm/AddToDoForm.js
+++ client/src/Containers/AddToDoForm/AddToDoForm.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class AddToDoForm extends React.Component {
     render() {
       return <div>
-      <label for="title"><b>Title</b></label>
-      <input type="text" placeholder="" name="title" required/>
+         <form className="LoginForm">
+      <label for="title"><b><FontAwesomeIcon icon="bell" /> Title</b></label>
+      <input type="text" placeholder="" name="title" required/><br />
       
-      <label for="place"><b>Place</b></label>
+      <label for="place"><FontAwesomeIcon icon="home" /><b> Place</b></label>
       <input type="text" placeholder="" name="place" required/>
       
-      <textarea rows="4" cols="50" name="notes" form="notesform">Notes</textarea>
-      
+      <textarea className="NoteBox" rows="8" cols="50" name="notes" form="notesform">Notes</textarea>
+      </form>
       <button type="submit">SAVE</button>
         </div>
 
