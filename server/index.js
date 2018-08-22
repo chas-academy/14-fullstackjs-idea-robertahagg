9094e60738d2d4cb9476c86a58258d2fa9ffcc73
Roberta Häggström <roberta.haggstrom@gmail.com>
2018-08-22 11:05:20 +0200

    Connection between backend and mongoDB now works. Still need to do some structuring

--- server/index.js
+++ server/index.js
@@ -5,7 +5,29 @@ const path = require('path');
 // For deploying, so client code is served by our backend server.
 //app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', (req, res) => res.send('Hello World!'))
+function getTodos(res) {
+    var MongoClient = require('mongodb').MongoClient
+
+    MongoClient.connect('mongodb://backend:h3lloyou@ds125272.mlab.com:25272/handly', function (err, client) {
+      if (err) throw err
+    
+      var db = client.db('handly')
+      
+      db.collection('todos').find().toArray(function (err, result) {
+        if (err) throw err
+    
+        console.log(result)
+        res.send(result);
+      })
+    })
+}
+
+app.get('/todos', (req, res) => getTodos(res))
+app.post('/todos', (req, res) => res.send('Hello World!'))
+app.get('/todos/:id', (req, res) => res.send('Hello World!'))
+app.put('/todos/:id', (req, res) => res.send('Hello World!'))
+app.delete('/todos/:id', (req, res) => res.send('Hello World!'))
+
 app.listen(3001, () => console.log('Example app listening on port 3001!'))
 
 //module.exports = app;
