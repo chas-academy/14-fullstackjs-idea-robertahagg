8cd52930b50df6b13e429ac9c5fec5d14c0e9eac
Roberta Häggström <roberta.haggstrom@gmail.com>
2018-08-30 19:01:14 +0200

    Now works with register new user to db. Still needs some adjustment.

---
+++ server/models/Users.js
@@ -0,0 +1,13 @@
+var mongoose = require("mongoose");
+
+var UserSchema = new mongoose.Schema({
+  username: { type: String, maxlength: [50, "Sorry your username is to long"] },
+  email: { type: String },
+  password: {
+    type: String[(4, "Paswwords must contain at least 4 characters")]
+  }
+});
+
+mongoose.model("Users", UserSchema);
+
+module.exports = mongoose.model("Users", UserSchema);
