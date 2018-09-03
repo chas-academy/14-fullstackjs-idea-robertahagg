4e7bbda7b617369e22dc521693f7f0d438677b6c
Roberta Häggström <roberta.haggstrom@gmail.com>
2018-09-03 21:52:45 +0200

    Restructure TodosList and ListView to match component and view architecture. https://reactpatterns.com/#container-component

--- client/src/Views/ListView/ListView.js
+++ client/src/Views/ListView/ListView.js
@@ -2,7 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import cityNewYork from "../../Img/cityNewYork.jpg";
 
-const ListView = () => {
+const ListView = props => {
+  let todoArray = props.todosInput;
+
   return (
     <div>
       <img className="BackGroundImg" src={cityNewYork} alt="handlyHeader" />
@@ -15,6 +17,12 @@ const ListView = () => {
       <Link to="/search">
         <button>Search</button>
       </Link>
+
+      <ul>
+        {todoArray.map(todo => (
+          <li key={todo._id}>{todo.title}</li>
+        ))}
+      </ul>
     </div>
   );
 };
