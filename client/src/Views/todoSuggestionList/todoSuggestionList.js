c523531327a1a68b518282ed730c13cdad53827c
Roberta Häggström <roberta.haggstrom@gmail.com>
2018-09-24 15:03:48 +0200

    Updating and deleting a single todo from search result suggestion list now works.

--- client/src/Views/todoSuggestionList/todoSuggestionList.js
+++ client/src/Views/todoSuggestionList/todoSuggestionList.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import Authentication from "../../Authentication";
 import "./style.css";
 
 const TodoSuggestionList = props => {
   const suggestions = props.results.map(todos => (
     <ul className="TodoSearchListArray">
       <li key={todos.id}>
-        {todos.title}{" "}
+        <Link to={`/todos/${todos._id}`}>{todos.title} </Link>
+
         <b className="BtnDone">
           <FontAwesomeIcon icon="check-circle" /> &nbsp; Done &nbsp;
         </b>
